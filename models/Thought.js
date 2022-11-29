e404367ca4e70039101a8d4833df3077dd74bf2d
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-28 20:44:07 -0800

    input parameters for thought schema

---
+++ models/Thought.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 }, 
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: { type: String, required: true },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    });
